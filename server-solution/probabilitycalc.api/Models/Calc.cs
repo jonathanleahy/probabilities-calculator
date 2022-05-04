04497a11eda8da6bbaaeb20614494fe8d2ae63d7
Jon Leahy <jonathanl@newtimesystems.com>
2022-05-04 23:51:28 +0100

    fix: tidying up folders

---
+++ server-solution/probabilitycalc.api/Models/Calc.cs
@@ -0,0 +1,9 @@
+namespace probabilitycalc.api.Models;
+
+public class Calc
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    // public string Params { get; set; } 
+    public double Result { get; set; }
+}
